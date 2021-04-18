b7eb9bf6adef327cef53987041796da640752759
andrew <huandrew@gmail.com>
2021-04-18 02:13:02 -0500

    remove unnecessary styling and properities on coinData obj

--- src/config/coinData.js
+++ src/config/coinData.js
@@ -11,58 +11,39 @@ const CoinList = [
 
 const CoinData = {
   BTC: {
-    id: "bitcoin", // poor nomenclature. need to check original intent before changing
     name: "Bitcoin",
-    WIF: "80",
-    addr: "00",
     COIN: "btc",
     NETWORK: "main",
     addrPrefixes: ["1", "3"], //todo: add support for bech32 addrs
   },
   BTCt: {
-    id: "bitcoin",
     name: "Bitcoin Testnet",
-    WIF: "ef",
-    addr: "6F",
     COIN: "btc",
     NETWORK: "test3",
     addrPrefixes: ["n", "m"],
   },
   BCY: {
-    id: "bitcoin",
     name: "Blockcypher",
-    WIF: "ef",
-    addr: "1B",
     COIN: "bcy",
     NETWORK: "test",
     addrPrefixes: ["B", "C"],
   },
   LTC: {
-    id: "litecoin",
     name: "Litecoin",
-    WIF: "b0",
-    addr: "30",
     COIN: "ltc",
     NETWORK: "main",
   },
   DOGE: {
-    id: "dogecoin",
     name: "Dogecoin",
-    WIF: "9e",
-    addr: "1e",
     COIN: "doge",
     NETWORK: "main",
   },
   DASH: {
-    id: "dash",
     name: "Dash",
-    WIF: "cc",
-    addr: "4c",
     COIN: "dash",
     NETWORK: "main",
   },
   ETH: {
-    id: "eth",
     name: "Ethereum",
     COIN: "eth",
     NETWORK: "main",
@@ -70,7 +51,6 @@ const CoinData = {
   },
   //blockcypher specific testnet
   bETH: {
-    id: "bETH",
     name: "Ethereum BC Testnet",
     COIN: "beth",
     NETWORK: "test",
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+import "styles/index.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import "styles/index.css";
 import ThemedApp from "./app/ThemedApp";
 import { Provider } from "react-redux";
 import store from "redux/store.js";
